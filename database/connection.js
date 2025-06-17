868918d3c1d6dc9c75567c5db12f8d5bc9be6390
emondaca <30303704+emondaca@users.noreply.github.com>
2025-06-17 01:31:06 -0400

    NewUpgrade

--- database/connection.js
+++ database/connection.js
@@ -3,11 +3,11 @@ import "dotenv/config"
 
 const { Pool } = pg;
 export const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: '@EMmc1890',
-    database: 'creaciones_mov',
-    port: 5433,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     allowExitOnIdle: true
 });
 
